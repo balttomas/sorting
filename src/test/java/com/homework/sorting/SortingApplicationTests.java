9ea4774b1d73728507bd0cbd196d9e4c0fea1710
Tomas Baltrukonis <tomas.baltrukonis@nortal.com>
2023-03-19 11:53:00 +0200

    instantiate project

---
+++ src/test/java/com/homework/sorting/SortingApplicationTests.java
@@ -0,0 +1,14 @@
+package com.homework.sorting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SortingApplicationTests {
+
+	@Test
+	void contextLoads() {
+		// should not fail when loading context
+	}
+
+}
