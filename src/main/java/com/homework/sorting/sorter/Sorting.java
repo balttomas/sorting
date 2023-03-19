3a4b0f6908ed24dcf57b134f36d88751bde2ca3d
Tomas Baltrukonis <tomas.baltrukonis@nortal.com>
2023-03-19 13:19:22 +0200

    javadoc on sorting public API

--- src/main/java/com/homework/sorting/sorter/Sorting.java
+++ src/main/java/com/homework/sorting/sorter/Sorting.java
@@ -2,6 +2,9 @@ package com.homework.sorting.sorter;
 
 import com.homework.sorting.domain.Student;
 
+/**
+ * A public API to sort students by performance.
+ */
 public interface Sorting {
 
   Student[] sort(Student[] students);
